35510c89b6332b30b9cb74055982b6feef4d97fa
snsebot <132944337+snsebot@users.noreply.github.com>
2023-05-08 23:49:41 +0300

    Create throwError.ts

---
+++ src/functions/throwError.ts
@@ -0,0 +1,18 @@
+/*
+
+throwError(path, line, code, msg);
+
+ option -  type  -  example
+
+  @path - string - index.ts;
+  @line - string - 1:18;
+  @code - number - 2001;
+  @msg  - string - Value is undefined.;
+
+*/
+
+import pc from "picocolors";
+
+export function throwError(path: string, line: string, code: number, msg: string) {
+  return pc.blue(path) + pc.yellow(line) + " - " + pc.red("error ") + pc.gray("SJS" + code + ": ") + msg
+}
